71230ac84efed0cc911dd3a82d073579200a5b73
hasparus <hasparus@gmail.com>
2018-09-26 08:03:20 +0200

    Add microbundle and build.

---
+++ build/RoughReactLogo/index.d.ts
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.css";
+declare const defaultProps: {
+    scale: number;
+    config: {
+        roughness: number;
+    };
+};
+declare type Props = typeof defaultProps & React.AllHTMLAttributes<SVGSVGElement>;
+declare class RoughReactLogo extends React.PureComponent<Props> {
+    static defaultProps: {
+        scale: number;
+        config: {
+            roughness: number;
+        };
+    };
+    svg: React.RefObject<SVGSVGElement>;
+    _mountShapes(): void;
+    componentDidMount(): void;
+    componentDidUpdate(prevProps: Props): void;
+    render(): JSX.Element;
+}
+export default RoughReactLogo;
---
+++ build/index.d.ts
@@ -0,0 +1,2 @@
+export { default } from "./RoughReactLogo";
+export * from "./RoughReactLogo";
