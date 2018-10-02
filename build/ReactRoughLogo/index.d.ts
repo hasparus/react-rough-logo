873b30b2c3f472aa2d0ae0ba24ce6d7d88c40a97
hasparus <hasparus@gmail.com>
2018-10-02 13:47:20 +0200

    Rebuild.

--- build/ReactRoughLogo/index.d.ts
+++ build/ReactRoughLogo/index.d.ts
@@ -1,19 +1,30 @@
 import React from "react";
 import "./styles.css";
-declare const defaultProps: {
-    scale: number;
-    config: {
-        roughness: number;
-    };
+declare type RoughOptions = {
+    roughness?: number;
+    bowing?: number;
+    stroke?: string;
+    strokeWidth?: number;
+    fill?: string;
+    fillStyle?: "hachure" | "solid" | "zigzag" | "cross-hatch" | "dots";
+    fillWeight?: number;
+    hachureAngle?: number;
+    hachureGap?: number;
+    curveStepCount?: number;
+    simplification?: number;
 };
-declare type Props = typeof defaultProps & React.AllHTMLAttributes<SVGSVGElement>;
+declare const defaultConfig: {
+    scale: number;
+    ellipsesOptions: RoughOptions;
+    circleOptions: RoughOptions;
+};
+declare type Props = typeof defaultConfig & React.SVGProps<SVGSVGElement>;
 declare class ReactRoughLogo extends React.PureComponent<Props> {
     static displayName: string | null;
     static defaultProps: {
         scale: number;
-        config: {
-            roughness: number;
-        };
+        ellipsesOptions: RoughOptions;
+        circleOptions: RoughOptions;
     };
     svg: React.RefObject<SVGSVGElement>;
     _mountShapes(): void;
